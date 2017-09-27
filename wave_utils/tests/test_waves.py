7744400f80ca950df953bfb665ccffa1ac24eb57
Jens Hoepken <jens@sourceflux.de>
2017-09-27 23:40:57 +0200

    Remove copy creating variables
    * Use globally defined np.pi
    * Use globally defined waves.g instead of g

--- wave_utils/tests/test_waves.py
+++ wave_utils/tests/test_waves.py
@@ -9,11 +9,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import numpy as np
 
-from wave_utils import waves 
+from wave_utils import waves
 
-#some constants:
-g = 9.806
-pi = np.pi
 
 def test_import():
     """ is there anything there? """
@@ -21,15 +18,13 @@ def test_import():
 
 def test_celerity_deep():
     h = 10000 # very deep
-    omega = 2*pi/1 # short period wave
-    k = waves.wave_number(g, omega, h)   # short waves
-    C = waves.celerity(k, h, g)
-    assert C == g / omega # should be for deep water -- in this case exact!
-    
+    omega = 2*np.pi/1 # short period wave
+    k = waves.wave_number(waves.g, omega, h)   # short waves
+    C = waves.celerity(k, h, waves.g)
+    assert C == waves.g / omega # should be for deep water -- in this case exact!
+
 def test_celerity_shallow():
     h = 1 # shallow
-    k = 2*pi/10000 # long waves
-    C = waves.celerity(k, h, g)
-    assert np.allclose(C, np.sqrt(g * h), rtol=1e-7) # should be for shallow
-
-
+    k = 2*np.pi/10000 # long waves
+    C = waves.celerity(k, h, waves.g)
+    assert np.allclose(C, np.sqrt(waves.g * h), rtol=1e-7) # should be for shallow
